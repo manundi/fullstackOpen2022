e675f4a96b1e42c0e1350eaaf5f25a9bef639952
Miika Anundi <manundi@gmail.com>
2022-10-02 16:43:30 +0300

    -

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick,text}) =>(
+    <button onClick={handleClick}> {text}</button>
+)
 
 const App = () => {
 const [counter, setCounter] = useState(0)
 
-const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({handleClick,text}) =>{
-    <button onClick={handleClick}> {text}</button>
-}
 const increaseCounter = () =>  setCounter(counter+1)
 const decreaseCounter = () =>  setCounter(counter-1)
 const zeroCounter = () => setCounter(0)
@@ -15,6 +16,7 @@ const zeroCounter = () => setCounter(0)
 return(
 <>
   <Display counter= {counter}/>
+  mitö
   <Button handleClick={increaseCounter} text="lisaa"/> 
   <Button handleClick={zeroCounter} text="nollaa"/> 
   <Button handleClick={decreaseCounter} text="vahenna"/> 
