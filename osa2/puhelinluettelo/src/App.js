98e2a86b62de973797f14f092ebfd81c7f05abf9
Miika Anundi <manundi@gmail.com>
2022-10-26 22:13:49 +0300

    2.16

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,55 +2,64 @@ import { useState, useEffect } from "react";
 import AddPersonForm from "./AddPersonForm";
 import PersonsList from "./PersonsList";
 import FilterForm from "./FilterForm";
-import axios from "axios"
+import contactsService from "./services/contacts";
+
 const App = () => {
-const [persons,setPersons] = useState([
-  {name : "Arto Hellas"},  {name : "Drtdaso Hellas"},  {name : "Bffrto Hellas"}
-])
-const [personsToShow,setPersonsToShow] = useState(persons)
-const [newName,setNewName] = useState('')
-const [newTel,setNewTel] = useState('')
+  const [persons,setPersons] = useState([])
+  const [personsToShow,setPersonsToShow] = useState([])
+  const [newName,setNewName] = useState('')
+  const [newTel,setNewTel] = useState('')
 
-const handleNameChange = (event) =>{
-  event.preventDefault()
-  setNewName(event.target.value)
-}
-const handleTelChange = (event) =>{
-  event.preventDefault()
-  setNewTel(event.target.value)
-}
-const handleSubmit = (event) =>{
-  event.preventDefault()
-  let newPerson = {}
-  newPerson.name = newName
-  newPerson.tel = newTel
-  if(persons.find(person => person.name === newPerson.name)){
-    alert(`${newPerson.name} is already in contacts`)
-    return
+  const handleNameChange = (event) =>{
+    event.preventDefault()
+    setNewName(event.target.value)
   }
-  let newPersons = persons.concat(newPerson)
-  setPersons(newPersons)
-  setPersonsToShow(newPersons)
-  event.target.reset()
-}
 
-const handleFilter = (event) =>{
-  event.preventDefault()
-  let filteredPersons= persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
-  setPersonsToShow(filteredPersons)
-}
-useEffect(() =>{
-  console.log("efekti")
-  axios
-    .get("http://localhost:3001/persons")
-    .then(res =>{
-      console.log(res.data)
-      setPersons(res.data)
-      setPersonsToShow(res.data)
-    })
+  const handleTelChange = (event) =>{
+    event.preventDefault()
+    setNewTel(event.target.value)
+  }
 
-    
-},[])
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    let newPerson = {}
+    newPerson.name = newName
+    newPerson.tel = newTel
+    if(persons.find(person => person.name === newPerson.name)){
+      alert(`${newPerson.name} is already in contacts`)
+      return
+    }
+    //let newPersons = persons.concat(newPerson)
+
+    contactsService.create(newPerson)
+      .then(response =>
+        setPersons(persons.concat(response))
+        )
+      .then(res => console.log(res))
+      .catch(error => alert(error))
+    event.target.reset()
+  }
+
+  const handleFilter = (event) =>{
+    event.preventDefault()
+    let filteredPersons = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setPersonsToShow(filteredPersons)
+  }
+
+  useEffect( () =>{
+    console.log("Perons muttui")
+    console.log(persons)
+    setPersonsToShow(persons)
+  },[persons])
+
+  useEffect(() =>{
+    console.log("efekti")
+    contactsService.getAll()
+      .then(initialContacts => setPersons(initialContacts))
+      .catch(error => alert(error.data))
+      
+  },[])
+  
   return (
   <>
     <h2>Phoneboook</h2>
